7a588cdfae599813e6cb9390306bf19d423a97d2
Camellloper <gencdeveloperci34@outlook.com>
2022-02-26 22:43:43 +0200

    t

--- Source/functions/sayac/sayac.py
+++ Source/functions/sayac/sayac.py
@@ -1,1 +1,15 @@
 # YKS-LGS Sayac
+import requests
+import json
+
+def sayacYks():
+    url = "http://www.canermastan.com:8080/yks"
+    response = requests.request("GET",url)
+    yks = json.loads(response.text)
+    return yks
+
+def sayacLgs():
+    url = "http://www.canermastan.com:8080/lgs"
+    response = requests.request("GET",url)
+    lgs = json.loads(response.text)
+    return lgs
